db7a2633477688d34374c2e76d2a15b9107c4ac1
Filip Burski <92650529+FlickJS@users.noreply.github.com>
2023-08-03 21:28:23 +0200

    change folder name

---
+++ src/utils/Algorithm.ts
@@ -0,0 +1,41 @@
+class Algorithm {
+    private array: number[];
+    private steps: number[][] = []; 
+
+    constructor(array: number[]) {
+        this.array = array;
+    }
+
+    quickSort(left: number = 0, right: number = this.array.length - 1) {
+        if (left < right) {
+            let pivotIndex = this.partition(left, right);
+
+            this.quickSort(left, pivotIndex - 1);
+            this.quickSort(pivotIndex + 1, right);
+        }
+        return this.steps;
+    }
+
+    private partition(left: number, right: number): number {
+        let pivotElement = this.array[right];
+        let pivotIndex = left;
+
+        for (let i = left; i < right; i++) {
+            if (this.array[i] < pivotElement) {
+                this.swap(i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+
+        this.swap(pivotIndex, right);
+        this.steps.push([...this.array]); 
+        return pivotIndex;
+    }
+
+    private swap(i: number, j: number) {
+        let temp = this.array[i];
+        this.array[i] = this.array[j];
+        this.array[j] = temp;
+    }
+}
+export default Algorithm;
